ff5943741deb60475f791f6249d421408ac07497
Min Hein Zaw <minheinzaw@gmail.com>
2023-09-22 12:44:39 +0630

    added constant file

---
+++ frontend/constant.py
@@ -0,0 +1,1 @@
+SKILL_CONTENT = " I have studed following Language. These are my skills for studying Language."
--- frontend/views.py
+++ frontend/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from dashboard.models import *
 from datetime import datetime
 import json
+from . import constant
 
 
 # Create your views here.
@@ -12,5 +13,13 @@ def index(request):
     age = today.year - birthday.year - ((today.month, today.day) < (birthday.month, birthday.day))
     text= TypeWriter.objects.values_list("text",flat=True)
     type_writers = json.dumps(list(text))
-    context = {'about':about, 'age':age, 'type_writers':type_writers}
+    skills = Skill.objects.all()
+    skill_content = constant.SKILL_CONTENT  ### for skill content
+    context = {
+        'about':about, 
+        'age':age, 
+        'type_writers':type_writers, 
+        'skills':skills, 
+        'skill_content':skill_content
+        }
     return render(request, 'frontend/index.html', context)
